095cc53dd65a626d8ab33c1fa042a6d91657c366
Cristina Saura <csaurape@everis.com>
2019-07-24 14:24:39 +0200

    ADD/UPDATE [MarkersResponseModel + bcnLocations.json] - added new files

---
+++ MapsApp/Maps/MarkersResponseModel.swift
@@ -0,0 +1,9 @@
+//
+//  MarkersResponseModel.swift
+//  mapsApp
+//
+//  Created by Cristina Saura Pérez on 19/07/2019.
+//  Copyright © 2019 Cristina Saura Pérez. All rights reserved.
+//
+
+import Foundation
