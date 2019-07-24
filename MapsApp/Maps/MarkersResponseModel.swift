c343c3566b19bef68eb3800c8b6c40e7d1c0ad58
Cristina Saura <csaurape@everis.com>
2019-07-24 14:27:42 +0200

    UPDATE [MarkersResponseModel] - The markers are extracted from a JSON !!!

--- MapsApp/Maps/MarkersResponseModel.swift
+++ MapsApp/Maps/MarkersResponseModel.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct Marker {
+    let name: String
+    let coordinates: [Double]
+}
+
+struct Markers {
+    var markers: [Marker]
+}
+
+extension Markers {
+    init?(json: [String: Any]) {
+        
+        guard let markersJSON = json["markers"] as? [[String: Any]] else { return nil }
+    
+        var markersObtanied: [Marker] = []
+        
+        for markerJSON in markersJSON {
+            
+            if let name = markerJSON["name"] as? String,
+                let coordinates = markerJSON["coordinates"] as? [Double] {
+                let newMarker = Marker.init(name: name, coordinates: coordinates)
+                markersObtanied.append(newMarker)
+            }
+        }
+        
+        self.markers = markersObtanied
+    }
+}
