36229a6fe446d5c4ba4df45eaecf631332949c9c
Adrià González Fernández <adria.gonzalez.fernandez@everis.com>
2019-07-25 09:27:14 +0200

    UPDATE: añadir present de realidad augmentada

---
+++ MapsApp/AR-Street/GiroscopioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GiroscopioViewController.swift
+//  mapsApp
+//
+//  Created by Adrià González Fernández on 23/07/2019.
+//  Copyright © 2019 Cristina Saura Pérez. All rights reserved.
+//
+
+import UIKit
+
+class GiroscopioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
