c16024a7556ef0d8f0e78a42e527edf49e4d36b2
Cristina Saura <csaurape@everis.com>
2019-08-13 11:19:18 +0200

    ADD [HalfModal] - ready to show half modal view controller

---
+++ MapsApp/Maps/HalfModalPresentationController.swift
@@ -0,0 +1,27 @@
+//
+//  HalfModalPresentationController.swift
+//  mapsApp
+//
+//  Created by Cristina Saura Pérez on 13/08/2019.
+//  Copyright © 2019 Cristina Saura Pérez. All rights reserved.
+//
+
+import Foundation
+
+//protocol HalfModalPresentable { }
+//
+//extension HalfModalPresentable where Self: UIViewController {
+//    func maximizeToFullScreen() -> Void {
+////        if let presetation = navigationController?.presentationController as? HalfModalPresentationController {
+////            presetation.changeScale(to: .adjustedOnce)
+////        }
+//    }
+//    
+////    func isHalfModalMaximized() -> Bool {
+////        if let presentationController = presentationController as? HalfModalPresentationController {
+////            return presentationController.isMaximized
+////        }
+////
+////        return false
+////    }
+//}
