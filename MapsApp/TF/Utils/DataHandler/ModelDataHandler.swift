c57b483fcba02e97184ab214b1a94155d2d6190a
Carles Cañadas Torrents <ccanadas@everis.com>
2019-08-13 13:49:21 +0200

    UPDATE : [SomeFiles] - For add apache license

--- MapsApp/TF/Utils/DataHandler/ModelDataHandler.swift
+++ MapsApp/TF/Utils/DataHandler/ModelDataHandler.swift
@@ -1,10 +1,16 @@
+// Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 //
-//  ModelDataHandler.swift
-//  mapsApp
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Carles Cañades Torrents on 13/08/2019.
-//  Copyright © 2019 Cristina Saura Pérez. All rights reserved.
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import CoreImage
 import TensorFlowLite
@@ -231,7 +237,7 @@ class ModelDataHandler {
             rgbBytes[index] = component.element
             index += 1
         }
-        if isModelQuantized { return Data(bytes: rgbBytes) }
+        if isModelQuantized { return Data(_: rgbBytes) }
         return Data(copyingBufferOf: rgbBytes.map { Float($0) / 255.0 })
     }
 }
