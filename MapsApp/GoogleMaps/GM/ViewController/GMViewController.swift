ea2fc28b700a8944b562c6b8ce424d8073ab93ed
Carles Cañadas Torrents <ccanadas@everis.com>
2019-08-22 13:14:37 +0200

    ADD : [AlertDialogService, LocationService] - For location and alert

--- MapsApp/GoogleMaps/GM/ViewController/GMViewController.swift
+++ MapsApp/GoogleMaps/GM/ViewController/GMViewController.swift
@@ -18,8 +18,8 @@ class GMViewController: UIViewController {
 
     // MARK: - Fields
 
-    private let locationManager = CLLocationManager()
-
+    private var locationService: LocationService?
+    private var markers: Markers?
     // MARK: - Constructor
 
     // MARK: - Life cycle
@@ -32,25 +32,65 @@ class GMViewController: UIViewController {
     public override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
+        locationService = LocationService()
+
         setup()
     }
 
     // MARK: - Helpers
-    
+
     func setup() {
         initializeLocationManager()
+        setupMap()
     }
     
     func initializeLocationManager() {
 
-        locationManager.requestWhenInUseAuthorization()
+        guard let locationService = locationService else {
+            return
+        }
 
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
-            locationManager.startUpdatingLocation()
+        locationService.setLocationManagerDelegate(delegate: self)
+    }
+    
+    func setupMap() {
+        guard let path = Bundle.main.path(forResource: "bcnlocations", ofType: "json") else {
+            return
+        }
+        
+        let fileUrl = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: fileUrl)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            
+            guard let array = json as? [String: Any] else { return }
+            self.markers = Markers.init(json: array)
+            setAnnotationsInMap()
+        } catch {
+            print(error)
         }
     }
+    
+    fileprivate func setAnnotationsInMap() {
+        
+        guard let markersArray = markers?.markers else {
+            return
+        }
+
+        for marker in markersArray {
+            //TODO first / last i'm not sure if can be done better
+            guard let latitude = marker.coordinates.first,
+                let longitude = marker.coordinates.last else {
+                return
+            }
+
+            let position = CLLocationCoordinate2DMake(latitude, longitude)
+            let gmsMarker = GMSMarker(position: position)
+            gmsMarker.title = marker.name
+            gmsMarker.map = mapView
+        }
+    }
+
 }
 
 extension GMViewController: CLLocationManagerDelegate {
@@ -63,6 +103,8 @@ extension GMViewController: CLLocationManagerDelegate {
 
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
+
+        cameraMoveToLocation(toLocation: manager.location?.coordinate)
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -80,11 +122,11 @@ extension GMViewController: CLLocationManagerDelegate {
 extension GMViewController {
 
     func cameraMoveToLocation(toLocation: CLLocationCoordinate2D?) {
-        
+
         guard let location = toLocation else {
             return
         }
-        
+
         mapView.camera = GMSCameraPosition(target: location,
                                            zoom: 15,
                                            bearing: .zero,
