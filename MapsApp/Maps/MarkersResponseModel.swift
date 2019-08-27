54fc3f00b7d62d58cff274457c4cc803ffd7a28d
Cristina Saura <csaurape@everis.com>
2019-08-27 09:23:14 +0200

    UPDATE - MarkersResponseModel

--- MapsApp/Maps/MarkersResponseModel.swift
+++ MapsApp/Maps/MarkersResponseModel.swift
@@ -7,32 +7,97 @@
 //
 
 import Foundation
+import UIKit
 
-struct Marker {
+struct Marker: Decodable {
     let name: String
     let coordinates: [Double]
+    let cars: [Car]
 }
 
-struct Markers {
-    var markers: [Marker]
+struct Car: Decodable {
+    let brand : String
+    let model : String
+    let year : String
+    let image: String
 }
 
-extension Markers {
-    init?(json: [String: Any]) {
-        
-        guard let markersJSON = json["markers"] as? [[String: Any]] else { return nil }
+let markers: [Marker] = load("bcnlocations.json")
+
+func load<T: Decodable>(_ filename: String, as type: T.Type = T.self) -> T {
+    let data: Data
     
-        var markersObtanied: [Marker] = []
-        
-        for markerJSON in markersJSON {
-            
-            if let name = markerJSON["name"] as? String,
-                let coordinates = markerJSON["coordinates"] as? [Double] {
-                let newMarker = Marker.init(name: name, coordinates: coordinates)
-                markersObtanied.append(newMarker)
-            }
-        }
-        
-        self.markers = markersObtanied
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        var result =  try decoder.decode(T.self, from: data)
+        return result
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
+//final class ImageStore {
+//    typealias _ImageDictionary = [String: UIImage]
+//    fileprivate var images: _ImageDictionary = [:]
+//
+//    fileprivate static var scale = 2
+//    
+//    static var shared = ImageStore()
+//    
+//    func image(name: String) -> Image {
+//        let index = _guaranteeImage(name: name)
+//        
+//        return Image(images.values[index], scale: CGFloat(ImageStore.scale), label: Text(verbatim: name))
+//    }
+//
+//    static func loadImage(name: String) -> CGImage {
+//        guard
+//            let url = Bundle.main.url(forResource: name, withExtension: "jpg"),
+//            let imageSource = CGImageSourceCreateWithURL(url as NSURL, nil),
+//            let image = CGImageSourceCreateImageAtIndex(imageSource, 0, nil)
+//        else {
+//            fatalError("Couldn't load image \(name).jpg from main bundle.")
+//        }
+//        return image
+//    }
+//    
+//    fileprivate func _guaranteeImage(name: String) -> _ImageDictionary.Index {
+//        if let index = images.index(forKey: name) { return index }
+//        
+//        images[name] = ImageStore.loadImage(name: name)
+//        return images.index(forKey: name)!
+//    }
+//}
+
+
+
+//extension Markers {
+//    init?(json: [String: Any]) {
+//
+//        guard let markersJSON = json["markers"] as? [[String: Any]] else { return nil }
+//
+//        var markersObtanied: [Marker] = []
+//
+//        for markerJSON in markersJSON {
+//
+//            if let name = markerJSON["name"] as? String,
+//                let coordinates = markerJSON["coordinates"] as? [Double] {
+//                let newMarker = Marker.init(name: name, coordinates: coordinates)
+//                markersObtanied.append(newMarker)
+//            }
+//        }
+//
+//        self.markers = markersObtanied
+//    }
+//}
