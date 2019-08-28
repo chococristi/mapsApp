2078b2bb1ee60aa3c78124c91c4a18a673d50fa1
Cristina Saura <csaurape@everis.com>
2019-08-28 11:03:35 +0200

    ADD/UPDATE [AnalyticsManager] - added analytics

---
+++ MapsApp/Firebase/AnalyticsManager.swift
@@ -0,0 +1,9 @@
+//
+//  AnalyticsManager.swift
+//  mapsApp
+//
+//  Created by Cristina Saura Pérez on 28/08/2019.
+//  Copyright © 2019 Cristina Saura Pérez. All rights reserved.
+//
+
+import Foundation
