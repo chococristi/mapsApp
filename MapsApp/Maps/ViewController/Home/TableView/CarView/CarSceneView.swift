8b2d732834f7f29d1fb60ac2481f3cafdd249a8e
Cristina Saura <csaurape@everis.com>
2019-08-30 12:57:02 +0200

    UPDATE [CarSceneView] - this view is showed

--- MapsApp/Maps/ViewController/Home/TableView/CarView/CarSceneView.swift
+++ MapsApp/Maps/ViewController/Home/TableView/CarView/CarSceneView.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class CarSceneView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var contentView: UIView!
+
+    // MARK: - View Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        Bundle.main.loadNibNamed(String(describing: CarSceneView.self), owner: self, options: nil)
+        contentView.fixInView(self)
+    }
 
 }
