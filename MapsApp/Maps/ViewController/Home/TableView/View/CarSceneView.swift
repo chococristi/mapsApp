5c5123b77f28d7f3560a40be54f130f9e02e3e87
Cristina Saura <csaurape@everis.com>
2019-09-03 11:29:12 +0200

    ADD / UPDATE [CarSceneView / AppIcon] - new icon and new car scene view

---
+++ MapsApp/Maps/ViewController/Home/TableView/View/CarSceneView.swift
@@ -0,0 +1,54 @@
+//
+//  CarSceneView.swift
+//  mapsApp
+//
+//  Created by Cristina Saura Pérez on 02/09/2019.
+//  Copyright © 2019 Cristina Saura Pérez. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class CarSceneView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var carNameLabel: UILabel!
+    @IBOutlet weak var sceneView: SCNView!
+
+    var node = SCNNode()
+
+    // MARK: - View Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+            Bundle.main.loadNibNamed(String(describing: CarSceneView.self), owner: self, options: nil)
+            contentView.fixInView(self)
+            sceneSetup()
+    }
+
+       func sceneSetup() {
+            let newScene = SCNScene()
+
+           let ambientLightNode = SCNNode()
+           ambientLightNode.light = SCNLight()
+           ambientLightNode.light!.type = SCNLight.LightType.ambient
+           ambientLightNode.light!.color = UIColor(white: 0.67, alpha: 1.0)
+           newScene.rootNode.addChildNode(ambientLightNode)
+            self.sceneView.scene = newScene
+        }
+
+    func init3DObject(node: SCNNode, car: Car) {
+        carNameLabel.text = car.brand + " " + car.model
+            self.node = node
+            self.sceneView.scene?.rootNode.addChildNode(node)
+        }
+
+}
